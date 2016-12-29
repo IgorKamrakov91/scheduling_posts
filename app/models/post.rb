ef2e5920e669e11009900c92a22066c62823ac30
IgorKamrakov <i.kamrakov@yandex.ru>
2016-12-29 11:28:21 +0300

    Add post_helper method & select field

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   scope :draft, -> { where(published_at: nil)   }
   scope :published, -> { where.not(published_at: nil).where("published_at <= ?", Time.zone.now)   }
   scope :scheduled, -> { where.not(published_at: nil).where("published_at > ?", Time.zone.now)   }
+
+  attr_accessor :status
 end
